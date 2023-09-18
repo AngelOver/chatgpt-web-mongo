d1ecd61b82764ccc295e7f2af6b36c4b6174ab61
zhangliang <2424123787@qq.com>
2023-09-18 12:37:24 +0800

    移除claude部分数据

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -22,12 +22,12 @@ const { HttpsProxyAgent } = httpsProxyAgent
 dotenv.config()
 
 const ErrorCodeMessage: Record<string, string> = {
-  401: '[OpenAI] 提供错误的API密钥 | Incorrect API key provided',
-  403: '[OpenAI] 服务器拒绝访问，请稍后再试 | Server refused to access, please try again later',
-  502: '[OpenAI] 错误的网关 |  Bad Gateway',
-  503: '[OpenAI] 服务器繁忙，请稍后再试 | Server is busy, please try again later',
-  504: '[OpenAI] 网关超时 | Gateway Time-out',
-  500: '[OpenAI] 服务器繁忙，请稍后再试 | Internal Server Error',
+  401: '[Claude] 提供错误的API密钥 | Incorrect API key provided',
+  403: '[Claude] 服务器拒绝访问，请稍后再试 | Server refused to access, please try again later',
+  502: '[Claude] 错误的网关 |  Bad Gateway',
+  503: '[Claude] 服务器繁忙，请稍后再试 | Server is busy, please try again later',
+  504: '[Claude] 网关超时 | Gateway Time-out',
+  500: '[Claude] 服务器繁忙，请稍后再试 | Internal Server Error',
 }
 
 let auditService: TextAuditService
