bd7d4476990407fa6cc24efb3d38b95afdff2140
zhangliang <2424123787@qq.com>
2023-09-18 21:09:56 +0800

    修改网站标题

--- replace-title.sh
+++ replace-title.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-SITE_TITLE=${SITE_TITLE:-ChatGPT Web}
+SITE_TITLE=${SITE_TITLE:-Claude AI}
 
 sed -i -E "s/<title>([^<]*)<\/title>/<title>${SITE_TITLE}<\/title>/g" /app/public/index.html
